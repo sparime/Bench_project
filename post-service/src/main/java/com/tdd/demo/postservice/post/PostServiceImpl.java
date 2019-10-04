e6c8d6fa48f223d01ca3b57c1a12ea5e6855f4ec
sourabhparime <sparime@solstice.com>
2019-10-04 13:25:32 -0500

    post service layer test skeleton
    Signed-off-by: sourabhparime <sparime@solstice.com>

--- post-service/src/main/java/com/tdd/demo/postservice/post/PostServiceImpl.java
+++ post-service/src/main/java/com/tdd/demo/postservice/post/PostServiceImpl.java
@@ -1,21 +1,51 @@
 package com.tdd.demo.postservice.post;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PostServiceImpl implements PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
     @Override
     public Post findById(int id) {
-        return null;
+        try {
+            return postRepository.findById(id).orElse(null);
+        } catch (Exception e) {
+            throw new RuntimeException("user not present");
+        }
     }
 
     @Override
     public int savePost(Post post) {
-        return 0;
+        try {
+            Post saved = postRepository.saveAndFlush(post);
+            return saved.getPostId();
+        } catch (Exception e) {
+            throw new RuntimeException("Could not be saved");
+        }
     }
 
     @Override
     public int updatePost(Post post) {
-        return 0;
+        try {
+            postRepository.saveAndFlush(post);
+            return postRepository.findById(post.getPostId()).orElse(null).getPostId();
+        } catch (Exception e) {
+            throw new RuntimeException("Could not update");
+        }
+    }
+
+    @Override
+    public int deletePost(int id) {
+
+        try {
+            postRepository.deleteById(id);
+            return id;
+        } catch (Exception e) {
+            throw new RuntimeException("Could not delete user");
+        }
     }
 }
--- post-service/src/test/java/com/tdd/demo/postservice/post/PostServiceLayerIntegrationTest.java
+++ post-service/src/test/java/com/tdd/demo/postservice/post/PostServiceLayerIntegrationTest.java
@@ -1,8 +1,30 @@
 package com.tdd.demo.postservice.post;
 
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 public class PostServiceLayerIntegrationTest {
+
+    @MockBean
+    private PostRepository postRepository;
+    @Autowired
+    private PostService postService;
+
+    @TestConfiguration
+    static class PostServiceTestContextConfiguration {
+
+        @Bean
+        public PostService postService() {
+            return new PostServiceImpl();
+        }
+    }
+    // set up before and/or after
+
+    // tests
+
 }
