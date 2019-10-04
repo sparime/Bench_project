fb958f556f4d5761274cccbdc58d9895bb470ec8
sourabhparime <sparime@solstice.com>
2019-10-04 16:16:47 -0500

    post controller layer test
    Signed-off-by: sourabhparime <sparime@solstice.com>

--- post-service/src/main/java/com/tdd/demo/postservice/post/PostController.java
+++ post-service/src/main/java/com/tdd/demo/postservice/post/PostController.java
@@ -1,7 +1,17 @@
 package com.tdd.demo.postservice.post;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PostController {
+    @Autowired
+    private PostService postService;
+
+    @GetMapping("/posts/{id}")
+    public Post getPost(@PathVariable int id) {
+        return postService.findById(id);
+    }
 }
--- post-service/src/test/java/com/tdd/demo/postservice/post/PostControllerTest.java
+++ post-service/src/test/java/com/tdd/demo/postservice/post/PostControllerTest.java
@@ -4,17 +4,24 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(PostController.class)
@@ -27,7 +34,7 @@ public class PostControllerTest {
     //private PostRepository postRepository;
 
     @Autowired
-    private MockMvc mockMvc;
+    private MockMvc mockmvc;
 
     public static String asJsonString(final Object obj) {
         try {
@@ -41,16 +48,12 @@ public class PostControllerTest {
     public void setUp() {
         Post post = new Post(12, 10, "Service layer test post");
         // mock all repo calls here
-        Mockito.when(postService.findById(post.getPostId())).thenReturn(post);
-        Mockito.when(postService.findByContent("Service layer test post")).thenReturn(post);
-        Mockito.when(postService.savePost(post)).thenReturn(post.getPostId());
-        Mockito.when(postService.exists(post.getPostId())).thenReturn(true);
+        //given(postService.findById(post.getPostId())).willReturn(post);
+        given(postService.findById(post.getPostId())).willReturn(post);
+        when(postService.findByContent("Service layer test post")).thenReturn(post);
+        when(postService.savePost(post)).thenReturn(post.getPostId());
+        when(postService.exists(post.getPostId())).thenReturn(true);
 
-    }
-
-    @Test
-    public void dummy_test() {
-        assertThat(postService.findById(12).getUserId()).isEqualTo(10);
     }
 
     @TestConfiguration
@@ -64,5 +67,28 @@ public class PostControllerTest {
 
     }
 
+    @Test
+    public void dummy_test() {
+        // hard coded values for the dummy test to check mocking
+        assertThat(postService.findById(12).getUserId()).isEqualTo(10);
 
+    }
+
+    // get mapping
+    @Test
+    public void getPostById_thenReturnThePost() throws Exception {
+        Post post = new Post(12, 10, "Service layer test post");
+        when(postService.findById(post.getPostId())).thenReturn(post);
+
+        mockmvc.perform(get("/posts/{id}", 12).
+                contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)).
+                andExpect(status().isOk()).
+                andExpect(jsonPath("$.*", hasSize(3))).
+                andExpect(jsonPath("$.postId", is(12))).
+                andExpect(jsonPath("$.userId", is(10))).
+                andExpect(jsonPath("$.postContent", is("Service layer test post")));
+
+    }
+
+    // delete mapping
 }
