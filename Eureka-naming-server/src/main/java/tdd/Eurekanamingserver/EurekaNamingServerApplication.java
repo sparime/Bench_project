405173601ee5ffe87a53f32b6b1827aecbe72937
sourabhparime <sparime@solstice.com>
2019-10-07 16:10:47 -0500

    eureka simple config

--- Eureka-naming-server/src/main/java/tdd/Eurekanamingserver/EurekaNamingServerApplication.java
+++ Eureka-naming-server/src/main/java/tdd/Eurekanamingserver/EurekaNamingServerApplication.java
@@ -1,13 +1,16 @@
-package com.tdd.demo.Eurekanamingserver;
+package tdd.Eurekanamingserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaNamingServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaNamingServerApplication.class, args);
 	}
+	// localhost:8761
 
 }
