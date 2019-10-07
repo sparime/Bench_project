a70a3cdc2ed011817aefddfc2674d0bc45fa95a3
sourabhparime <sparime@solstice.com>
2019-10-07 16:28:51 -0500

    Eureka and load balancing with Ribbon

--- post-service/src/main/java/com/tdd/demo/postservice/PostServiceApplication.java
+++ post-service/src/main/java/com/tdd/demo/postservice/PostServiceApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableDiscoveryClient
-@EnableFeignClients("com.tdd.demo.postservice")
+@EnableDiscoveryClient // for eureka and other discoveries
+@EnableFeignClients("com.tdd.demo.postservice") // feign
 @SpringBootApplication
 
 public class PostServiceApplication {
